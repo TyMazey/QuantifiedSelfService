05361e2ca920a1cef175cfe274819c36d6ba7d7b
plapicola <p.lapicola@gmail.com>
2019-05-12 19:39:52 -0600

    Adds controller for recipe search route

---
+++ controllers/recipes_search_controller.js
@@ -0,0 +1,14 @@
+var RecipesSearchIndexFacade = require('../facades/recipe_search_index_facade');
+
+module.exports = class RecipesSearchController {
+  static index(request, response) {
+    response.setHeader('Content-Type', 'application/json');
+    RecipesSearchIndexFacade.requestRecipes(request.query.food)
+    .then(facade => {
+      response.status(facade.status).send(facade.body);
+    })
+    .catch(facade => {
+      response.status(facade.status).send(facade.body);
+    })
+  }
+}
--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = require(express.Router());
 var RecipesSearchController = require('../../../controllers/recipes_search_controller');
+
 /* GET recipes for search term */
 router.get('/', RecipesSearchController.index);
 
