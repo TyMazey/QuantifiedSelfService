9d48f2206ecd5ac72c7c6e842a8854df60a79172
plapicola <p.lapicola@gmail.com>
2019-05-12 19:57:04 -0600

    Adds RecipeService

---
+++ services/recipe_service.js
@@ -0,0 +1,13 @@
+var fetch = require('node-fetch');
+const baseUrl = 'https://api.edamam.com/'
+
+module.exports = class RecipeService {
+  static requestRecipesForFood(food) {
+    return new Promise((resolve, reject) => {
+      fetch(`${baseUrl}/search?q=${food}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_API_KEY}`)
+      .then(response => response.json())
+      .then(result => resolve(result.hits))
+      .catch(error => reject(error))
+    })
+  }
+}
