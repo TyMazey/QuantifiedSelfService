13b7ae22f30f60746f3404a7eb98c87a7cb81c95
plapicola <p.lapicola@gmail.com>
2019-05-12 19:42:28 -0600

    Corrects naming of variable from location to food in facade and service

--- facades/recipes_search_index_facade.js
+++ facades/recipes_search_index_facade.js
@@ -8,10 +8,10 @@ module.exports = class RecipesSearchIndexFacade {
     this.body = body
   }
 
-  static requestRecipes(location) {
+  static requestRecipes(food) {
     return new Promise((resolve, reject) => {
-      location ?
-      findOrRequestRecipes(location) :
+      food ?
+      findOrRequestRecipes(food) :
       reject(new RecipesSearchIndexFacade(400, {error: "Food query parameter is required."}))
       .then(recipes => {
         resolve(new RecipesSearchIndexFacade(200, RecipeSerializer.formatAll(recipes)))
@@ -23,10 +23,10 @@ module.exports = class RecipesSearchIndexFacade {
   }
 }
 
-function findOrRequestRecipes(location) {
+function findOrRequestRecipes(food) {
   // Just requests pending discussion with team regarding intent for storage
   return new Promise((resolve, reject) => {
-    RecipeService.requestLocation(locaiton)
+    RecipeService.requestRecipesForFood(food)
     .then(recipes => {
       return Recipe.fromRequest(recipes)
     })
