a786e6abcf021443140e2a790d45fbe61a5e1b70
plapicola <p.lapicola@gmail.com>
2019-05-12 20:15:27 -0600

    Corrects require issue on router

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = require(express.Router());
+var router = express.Router();
 var RecipesSearchController = require('../../../controllers/recipes_search_controller');
 
 /* GET recipes for search term */
