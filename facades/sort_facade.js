435b6a9fa633193280ad8bc7d28ff08802750212
Ty Mazey <tymazey@gmail.com>
2019-05-14 14:03:16 -0600

    adds static function to facade, and function to check if search givin

--- facades/sort_facade.js
+++ facades/sort_facade.js
@@ -2,10 +2,20 @@ module.exports = class SortFacade {
   static sortRecipes(search) {
     checkSearch(search)
     .then(search => {
-
+      return sortSearchedCalories(search)
     })
     .catch(noSearch => {
-      
+      return sortAllCalories()
     })
   }
 }
+
+function checkSearch(search) {
+  return new Promise(function(resolve, reject) {
+    if (search){
+      resolve(search);
+    }else {
+      reject();
+    }
+  })
+};
